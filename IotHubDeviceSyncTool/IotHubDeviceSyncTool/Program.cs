71c8c084dc21ff548e41f298abf5add5a97bfccf
Mikhail Chatillon <chmikhai@microsoft.com>
2019-12-02 19:29:23 +0100

    Initial Commit

---
+++ IotHubDeviceSyncTool/IotHubDeviceSyncTool/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace IotHubDeviceSyncTool
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+
+            var configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json")
+              .AddEnvironmentVariables()
+              .Build();
+
+            Console.WriteLine(configuration.GetConnectionString("DestinationIotHubConnectionString"));
+            Console.WriteLine(configuration.GetConnectionString("BlobSasToken"));
+
+            RegistryManager sourceRegistryManager = RegistryManager.CreateFromConnectionString(configuration.GetConnectionString("SourceIotHubConnectionString"));
+            RegistryManager destinationRegistryManager = RegistryManager.CreateFromConnectionString(configuration.GetConnectionString("DestinationIotHubConnectionString"));
+
+            string containerSasUri = configuration.GetConnectionString("BlobSasToken");
+
+            Console.WriteLine($"Start export of IoT Hub devices to blob");
+            // Call an export job on the IoT Hub to retrieve all devices
+
+            JobProperties exportJob =
+              await sourceRegistryManager.ExportDevicesAsync(containerSasUri, false);
+            var exportJobResult = await WaitForJobCompletion(sourceRegistryManager, exportJob);
+
+            Console.WriteLine($"Start import of IoT Hub devices to blob");
+
+            JobProperties importJob =
+            await destinationRegistryManager.ImportDevicesAsync(containerSasUri, containerSasUri);
+
+            var importJobResult = await WaitForJobCompletion(destinationRegistryManager, importJob);
+        }
+
+        private static async Task<JobProperties> WaitForJobCompletion(RegistryManager registryManager, JobProperties currentJob)
+        {
+            while (true)
+            {
+                currentJob = await registryManager.GetJobAsync(currentJob.JobId);
+                if (currentJob.Status == JobStatus.Completed ||
+                    currentJob.Status == JobStatus.Failed ||
+                    currentJob.Status == JobStatus.Cancelled)
+                {
+                    // Job has finished executing
+                    break;
+                }
+
+                Console.WriteLine($"Job progress: {currentJob.Progress}%");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+            }
+            Console.WriteLine($"Job Completed with status {currentJob.Status}");
+            return currentJob;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # iothubdevicesynctool
+
+This tool migrate devices from an IoT Hub to another one. It i based on the [following documentation](https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-bulk-identity-mgmt).
+
+# Quickstart
+1. Create/use an existing storage account
+2. Create a shared access policies on the storage account blade on Azure and copy the storage account in appsettings.json. Append a blob name to the existring url
+3. Provision the iothub owner connection string in the appsettings.json
+4. Run the script
